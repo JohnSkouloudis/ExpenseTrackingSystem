2ac1aeca18fa53eedf2183cfd2be7393d3a8a184
John skoul <johnskoul56@gmail.com>
2025-03-22 14:32:51 +0200

    Updated Receipt.java,maybe add some changes

--- src/main/java/com/example/expensetrackingsystem/entities/Receipt.java
+++ src/main/java/com/example/expensetrackingsystem/entities/Receipt.java
@@ -1,4 +1,17 @@
 package com.example.expensetrackingsystem.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Receipt {
+
+    private String imagePath;
+
+    @Id
+    @OneToOne
+    @JoinColumn(name = "transaction_id")
+    private Transaction transaction;
 }
