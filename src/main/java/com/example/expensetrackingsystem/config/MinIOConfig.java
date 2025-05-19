8402b121108bc3ae9f80947667211da01ac32e72
John skoul <johnskoul56@gmail.com>
2025-05-19 18:42:05 +0300

    added maven dependency for MinIO and created MinIOConfig to connect to the object storage server

---
+++ src/main/java/com/example/expensetrackingsystem/config/MinIOConfig.java
@@ -0,0 +1,28 @@
+package com.example.expensetrackingsystem.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinIOConfig {
+
+    @Value("${MINIO_URL}")
+    private String URL;
+
+    @Value("${MINIO_USERNAME}")
+    private String username;
+
+    @Value("${MINIO_PASSWORD}")
+    private String password;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(URL)
+                .credentials(username, password)
+                .build();
+    }
+
+}
