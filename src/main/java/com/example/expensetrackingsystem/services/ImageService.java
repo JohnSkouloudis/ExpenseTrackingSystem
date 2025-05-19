ca576afae9aa0e347eee0b396c71c0af2fb00ad7
John skoul <johnskoul56@gmail.com>
2025-05-19 18:42:24 +0300

    Created ImageService

---
+++ src/main/java/com/example/expensetrackingsystem/services/ImageService.java
@@ -0,0 +1,73 @@
+package com.example.expensetrackingsystem.services;
+
+
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ImageService {
+
+
+    private final MinioClient minioClient;
+
+    @Autowired
+    public ImageService(MinioClient minioClient) {
+        this.minioClient = minioClient;
+    }
+
+    public void createBucket(String bucketName) {
+        try {
+
+            boolean exists = minioClient.bucketExists(
+                    BucketExistsArgs.builder().bucket(bucketName).build()
+            );
+
+            if (!exists) {
+
+                minioClient.makeBucket( MakeBucketArgs.builder().bucket(bucketName).build() );
+            }else {
+                System.out.println("Bucket "+ bucketName + " already exists.");
+            }
+
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating bucket: " + e.getMessage(), e);
+        }
+    }
+
+    public void uploadImage(String bucketName, String imageName, MultipartFile file)  {
+
+        try {
+
+            boolean exists = minioClient.bucketExists(
+                    BucketExistsArgs.builder().bucket(bucketName).build()
+            );
+
+            if(exists){
+                minioClient.putObject(
+                        PutObjectArgs.builder()
+                                .bucket(bucketName)
+                                .object(imageName)
+                                .stream(file.getInputStream(), file.getSize(), -1)
+                                .contentType(file.getContentType())
+                                .build()
+                );
+                System.out.println("Image uploaded successfully.");
+
+            }else {
+                System.out.println("Bucket "+ bucketName + " doesnt exist.");
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
