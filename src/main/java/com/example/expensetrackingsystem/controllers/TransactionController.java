119d6a72c8d3866c024c9a0badc9c8393aa78325
John skoul <johnskoul56@gmail.com>
2025-05-20 17:20:50 +0300

    Implemented endpoint to create a transaction and add image to MinIO

--- src/main/java/com/example/expensetrackingsystem/controllers/TransactionController.java
+++ src/main/java/com/example/expensetrackingsystem/controllers/TransactionController.java
@@ -1,14 +1,19 @@
 package com.example.expensetrackingsystem.controllers;
 
 import com.example.expensetrackingsystem.dto.TransactionDTO;
+import com.example.expensetrackingsystem.entities.Transaction;
 import com.example.expensetrackingsystem.services.ImageService;
+import com.example.expensetrackingsystem.services.ReceiptService;
 import com.example.expensetrackingsystem.services.TransactionService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -22,6 +27,9 @@ public class TransactionController {
 
     @Autowired
     private TransactionService transactionService;
+
+    @Autowired
+    private ReceiptService receiptService;
 
     // Endpoint to get transactions for a specific account
     @GetMapping("/{accountId}")
@@ -37,23 +45,28 @@ public class TransactionController {
         return ResponseEntity.ok(transactions);
     }
 
+
+
     // Endpoint to create a transaction for an account
-    @PostMapping("/add")
+    @PostMapping(value = "/add" , consumes = {MediaType.MULTIPART_FORM_DATA_VALUE} )
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<String> createTransaction( @RequestBody TransactionDTO transactiondto) {
-        transactionService.saveTransaction(transactiondto);
+    public ResponseEntity<String> createTransaction(@RequestPart("data")String transactionDto,
+                                                    @RequestPart(value = "image", required = false) MultipartFile image) throws JsonProcessingException {
+
+
+        TransactionDTO dto = transactionService.convertToTransactionDto(transactionDto);
+
+        Transaction transaction = transactionService.saveTransaction(dto);
+
+        if (!image.isEmpty()) {
+            receiptService.saveReceipt(transaction, image.getOriginalFilename());
+            imageService.uploadImage("john", image);
+
+        }
         return ResponseEntity.ok("Transaction created successfully");
     }
 
 
-    @PostMapping("/create-bucket")
-    public ResponseEntity<String> createBucket(@RequestParam String name) {
-        try {
-            imageService.createBucket(name);
-            return ResponseEntity.ok("Bucket created: " + name);
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error: " + e.getMessage());
-        }
-    }
+
 
 }
