275d501441ce35c5d2c1c19e2bf468f7273ce07f
John skoul <johnskoul56@gmail.com>
2025-06-10 01:30:49 +0300

    added BcryptPasswordEncoder

--- src/main/java/com/example/expensetrackingsystem/components/SwaggerDocumentation.java
+++ src/main/java/com/example/expensetrackingsystem/components/SwaggerDocumentation.java
@@ -1,19 +1,33 @@
 package com.example.expensetrackingsystem.components;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SwaggerDocumentation {
 
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+
     @Bean
     public OpenAPI openAPI() {
 
-       return new OpenAPI().info(new Info().title("Expense Tracking System").version("1.0")
+       return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                       addList("Bearer Authentication"))
+               .components(new Components().addSecuritySchemes
+                       ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("Expense Tracking System").version("1.0")
                 .description("API documentation for the Expense Tracking System")
                 .version("1.0")
                 .contact(new Contact().name("John Skouloudis").url("https://github.com/JohnSkouloudis/ExpenseTrackingSystem"))
@@ -21,6 +35,11 @@ public class SwaggerDocumentation {
         );
 
 
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
