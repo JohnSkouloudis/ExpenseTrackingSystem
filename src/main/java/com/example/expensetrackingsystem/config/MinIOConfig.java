32a6b2ffa50dac5e3b41972b0d446e1a62623eb1
John skoul <johnskoul56@gmail.com>
2025-06-10 01:26:44 +0300

    Added Logger

--- src/main/java/com/example/expensetrackingsystem/config/MinIOConfig.java
+++ src/main/java/com/example/expensetrackingsystem/config/MinIOConfig.java
@@ -1,12 +1,16 @@
 package com.example.expensetrackingsystem.config;
 
 import io.minio.MinioClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinIOConfig {
+
+    Logger logger = LoggerFactory.getLogger(MinIOConfig.class);
 
     @Value("${MINIO_URL}")
     private String URL;
@@ -18,7 +22,7 @@ public class MinIOConfig {
     private String password;
 
     @Bean
-    public MinioClient minioClient() {
+    public MinioClient minioClient()  {
         return MinioClient.builder()
                 .endpoint(URL)
                 .credentials(username, password)
