3e65b22660e0268deb8bd88fe7c144e722e44455
John skoul <johnskoul56@gmail.com>
2025-06-12 23:37:15 +0300

    Implemented BudgetListener

--- src/main/java/com/example/expensetrackingsystem/components/BudgetListener.java
+++ src/main/java/com/example/expensetrackingsystem/components/BudgetListener.java
@@ -1,4 +1,30 @@
 package com.example.expensetrackingsystem.components;
 
+import com.example.expensetrackingsystem.entities.Budget;
+import com.example.expensetrackingsystem.services.BudgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import com.example.expensetrackingsystem.entities.CategoryEnum;
+
+@Component
 public class BudgetListener {
+
+    @Autowired
+    private BudgetService budgetService;
+
+    @EventListener
+    public void UpdateBudgetRemainingAmount(TransactionCreationEvent event) {
+        int userId = event.getUserId();
+        int categoryId = event.getTransaction().getCategory().getId();
+
+        Budget budget = budgetService.getBudgetByUserId(userId);
+
+        if( budget != null &&  CategoryEnum.isCategoryExpense(categoryId) ) {
+
+            budgetService.updateBudgetRemainingAmount( budget, event.getTransaction().getAmount());
+
+        }
+
+    }
 }
