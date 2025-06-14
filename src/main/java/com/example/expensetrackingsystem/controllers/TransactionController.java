bb1764260f6a991368062123fc3d1ca20ac647af
John skoul <johnskoul56@gmail.com>
2025-06-14 18:15:22 +0300

    Added method findByEndDate

--- src/main/java/com/example/expensetrackingsystem/controllers/TransactionController.java
+++ src/main/java/com/example/expensetrackingsystem/controllers/TransactionController.java
@@ -12,7 +12,6 @@ import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
--- src/main/java/com/example/expensetrackingsystem/repositories/BudgetRepository.java
+++ src/main/java/com/example/expensetrackingsystem/repositories/BudgetRepository.java
@@ -5,8 +5,12 @@ import com.example.expensetrackingsystem.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface BudgetRepository extends JpaRepository<Budget, Integer> {
 
     Budget findByUserId(int userId);
+    List<Budget> findByEndDate(LocalDate endDate);
 }
